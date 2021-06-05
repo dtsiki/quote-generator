c88940b6fb909e95a733cd05e92b7bef38616e1e
thevioletmaniac <thevioletmaniac@gmail.com>
2021-06-05 12:43:50 +0600

    Add favourite quotes feature

---
+++ src/js/favouriteQuotes.js
@@ -0,0 +1,67 @@
+const favouritesBody = document.querySelector('.favourites');
+const favouritesClearButton = document.querySelector('.fav-clear');
+
+export const isQuoteAlreadyFavourite = (quote) => {
+  const favouriteQuotes = getFavourites();
+  if (favouriteQuotes) return favouriteQuotes.indexOf(quote) > -1;
+};
+
+export const clearFavouritesBody = () => {
+  favouritesBody.innerHTML = '';
+};
+
+export const showFavourites = () => {
+  const favouriteQuotes = getFavourites();
+
+  if (!favouriteQuotes) {
+    favouritesBody.innerHTML += `<li class="favourites-message">You don't have any favourite quote</li>`;
+    return;
+  }
+
+  favouriteQuotes.map((quote) => {
+    favouritesBody.innerHTML += `
+      <li class="favourites-item">
+        <button class="favourites-button" quote="${quote}">
+          <span class="favourites-button__label">Remove</span>
+        </button>
+        <span class="favourites-quote">${quote}</span>
+      </li>
+    `;
+  });
+};
+
+export const getFavourites = () => {
+  const data = localStorage.getItem('favouriteQuotes');
+
+  if (data) {
+    return JSON.parse(JSON.stringify(JSON.parse(data)));
+  }
+};
+
+export const removeFromFav = (quote) => {
+  const favouriteQuotes = getFavourites();
+
+  if (favouriteQuotes) {
+    favouriteQuotes.splice(favouriteQuotes.indexOf(quote), 1);
+    localStorage.setItem('favouriteQuotes', JSON.stringify(favouriteQuotes));
+    clearFavouritesBody();
+    showFavourites();
+  }
+};
+
+const clearFavourites = () => {
+  clearFavouritesBody();
+  showFavourites();
+  localStorage.removeItem('favouriteQuotes');
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  favouritesClearButton.onclick = clearFavourites;
+  let removeButtons = document.querySelectorAll('.favourites-button');
+
+  removeButtons.forEach((button) => {
+    button.onclick = () => {
+      removeFromFav(button.getAttribute('quote'));
+    };
+  });
+});
